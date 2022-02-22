302731b542ee5bca12c0baafb56b0f5ee8a34052
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-02-22 14:21:48 +0100

    feat(sam-agent): first version of feature vector

--- bomberman_rl/agent_code/sam_agent/callbacks.py
+++ bomberman_rl/agent_code/sam_agent/callbacks.py
@@ -14,6 +14,15 @@ from enum import Enum, auto
 BOMB_POWER = 3
 EXPLOSION_TIMER = 2
 
+### feature extraction parameter ###
+
+K_NEAREST_CRATES = 1
+K_NEAREST_COINS = 1
+K_NEAREST_EXPLOSIONS = 1
+K_NEAREST_BOMBS = 1
+
+####################################
+
 positions = np.empty((17, 17, 2))
 
 for i in range(17):
@@ -41,6 +50,8 @@ def act(self, game_state: dict) -> str:
     if not alive:
         return Actions.WAIT
 
+    print(state_to_features(game_state).shape)
+
     return Actions.WAIT
 
 
@@ -52,11 +63,16 @@ def state_to_features(game_state: dict):
     position = np.array(position)
 
     field = game_state["field"]
-    nearest_crates = k_closest(position, crate_positions(field), k=3)
+    nearest_crates = k_closest(position, crate_positions(field), k=K_NEAREST_CRATES)
     crates_direction = nearest_crates - position
+
+    x, y = position
+    # indicates whether top, left, down, right there is a wall next to agent
+    walls = np.array([field[x, y - 1], field[x - 1, y], field[x, y + 1], field[x + 1, y]]) == -1
 
     others = game_state["others"]
     others_positions = np.array([other[3] for other in others])
+    others_positions = pad_matrix(others_positions, 3)
     others_directions = others_positions - position
 
     bombs = game_state["bombs"]
@@ -64,20 +80,22 @@ def state_to_features(game_state: dict):
     bomb_direction = close_bombs - position
 
     coins = np.array(game_state["coins"])
-    closest_coins = k_closest(position, coins, k=3)
+    closest_coins = k_closest(position, coins, k=K_NEAREST_COINS)
     coin_directions = closest_coins - position
 
     explosion_map = game_state["explosion_map"].T
     explosion_positions = positions[explosion_map == 1]
-    explosion_positions = k_closest(position, explosion_positions, k=4)
+    explosion_positions = k_closest(position, explosion_positions, k=K_NEAREST_EXPLOSIONS)
     explosion_direction = explosion_positions - position
 
     return np.concatenate(
         [
             position,
+            walls,
             others_directions.flatten(),
             crates_direction.flatten(),
             coin_directions.flatten(),
+            bomb_direction.flatten(),
             explosion_direction.flatten(),
         ]
     )
@@ -93,7 +111,7 @@ def bomb_vector(position, bombs):
     """
 
     if len(bombs) == 0:
-        return np.zeros((4, 2))
+        return np.zeros((K_NEAREST_BOMBS, 2))
 
     bomb_positions = np.array([bomb[0] for bomb in bombs])
     bomb_steps = np.array([bomb[1] for bomb in bombs])
@@ -101,15 +119,16 @@ def bomb_vector(position, bombs):
 
     within_range = (distances - bomb_steps - 1 - BOMB_POWER - EXPLOSION_TIMER) < 1
     bomb_positions = bomb_positions[within_range]
-    bomb_positions = np.concatenate([bomb_positions, np.zeros((4 - bomb_positions.shape[0], 2))])
-
+    bomb_positions = k_closest(position, bomb_positions, k=K_NEAREST_BOMBS)
     return bomb_positions
 
 
 def k_closest(position, objects, k=5):
-    """Uses manhattan distance"""
+    """Always returns size k x 2 (pads with zeros). Uses manhattan distance"""
     if objects.shape[0] == 0:
-        return objects
+        return np.zeros((k, 2))
+
+    objects = pad_matrix(objects, k)
 
     distances = manhattan_distance(position, objects)
 
@@ -123,3 +142,10 @@ def crate_positions(field):
 def manhattan_distance(position, objects):
     """objects with shape D x 2"""
     return np.abs(objects - position).sum(axis=1)
+
+
+def pad_matrix(mat, size):
+    """Pads a N x 2 matrix to size x 2"""
+    N = mat.shape[0]
+    zero_rows_to_add = max(0, size - N)
+    return np.concatenate([mat, np.zeros((zero_rows_to_add, 2))])
