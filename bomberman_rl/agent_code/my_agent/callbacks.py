319c7aa92233cd4dd3e618a0695a57f3e9a06a5f
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-03-13 23:50:42 +0100

    chore: merge callbacks from main

--- bomberman_rl/agent_code/my_agent/callbacks.py
+++ bomberman_rl/agent_code/my_agent/callbacks.py
@@ -1,30 +1,12 @@
 import numpy as np
 from .learning_utilities import features_from_game_state
-from .features.feature import (
-    AgentInBlastZone,
-    AgentPosition,
-    BombDirection,
-    BombDropPossible,
-    BoxesInBlastRange,
-    CoinDirections,
-    CouldEscapeOwnBomb,
-    CrateDirection,
-    ExplosionDirections,
-    FeatureCollector,
-    MoveIntoDeath,
-    MoveNextToNearestBox,
-    MoveOutOfBlastZone,
-    MoveToNearestCoin,
-    OpponentDirections,
-    PossibleActions,
-    Walls,
-)
 from enum import Enum
 from joblib import dump, load
 import os
 import math
+from .path_utilities import FeatureExtraction, Actions
 
-
+# isn't this also set in learning utils?
 EPSILON = 0
 MODEL_PATH = "model.joblib"
 
@@ -39,6 +21,7 @@ class Actions(Enum):
 
 
 def setup(self):
+    self.past_moves = []
     if os.path.isfile(MODEL_PATH):
         self.trees = load(MODEL_PATH)
     self.EPSILON = EPSILON
@@ -46,48 +29,31 @@ def setup(self):
 
 def act(self, game_state: dict):
     if game_state["step"] == 1:
-        choice = np.random.choice(["RIGHT", "LEFT", "UP", "DOWN"])  # DO NOT PLACE BOMB IMMEDIATELY
-        return choice
-    # Exploit or explore according to the exploration probability
+        self.past_moves = []
     if np.random.randint(1, 100) / 100 < self.EPSILON:
-        return explore()
+        return explore(self)
     else:
         return exploit(self, game_state)
 
 
-def explore():
+def explore(self):
     choice = np.random.choice(["RIGHT", "LEFT", "UP", "DOWN", "WAIT", "BOMB"])
+    self.past_moves.append(choice)
     return choice
 
 
 def exploit(self, game_state):
-    feature_collector = FeatureCollector(
-        AgentPosition(),
-        BombDropPossible(),
-        ExplosionDirections(),
-        CoinDirections(),
-        OpponentDirections(),
-        Walls(),
-        CrateDirection(),
-        BombDirection(),
-        MoveToNearestCoin(),
-        MoveOutOfBlastZone(),
-        MoveNextToNearestBox(),
-        BoxesInBlastRange(),
-        AgentInBlastZone(),
-        PossibleActions(),
-        # MoveIntoDeath(),
-        CouldEscapeOwnBomb(),
-    )
+    feature_extration = FeatureExtraction(game_state, self.past_moves)
 
     best_prediction = "WAIT"
     best_prediction_value = -math.inf
     for action in Actions:
         action = action.name
-        features = feature_collector.compute_feature(game_state)
-        feature_collector.print_feature_summary(features)
+        features = np.array(features_from_game_state(self, feature_extration))
         prediction = self.trees[action].predict(features.reshape(1, -1))
         if best_prediction_value < prediction:
             best_prediction_value = prediction
             best_prediction = action
+
+    self.past_moves.append(best_prediction)
     return best_prediction
